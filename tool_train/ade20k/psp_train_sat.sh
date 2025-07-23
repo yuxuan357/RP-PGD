ecddaa5a20736b887b4980974d4488aa0950ac09
yuxuan357 <yxzhang0911@gmail.com>
2025-07-23 15:13:56 +0800

    Add files via upload

--- tool_train/ade20k/psp_train_sat.sh
+++ tool_train/ade20k/psp_train_sat.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 PYTHON=python
 
-dataset=cityscapes
+dataset=ade20k
 exp_name=pspnet50_sat
 exp_dir=exp/${dataset}/${exp_name}
 model_dir=${exp_dir}/model
@@ -10,10 +10,9 @@ config=config/${dataset}/${dataset}_${exp_name}.yaml
 now=$(date +"%Y%m%d_%H%M%S")
 
 mkdir -p ${model_dir} ${result_dir}
-cp tool_train/cityscapes/psp_train_sat.sh tool_train/train_sat_psp.py ${config} ${exp_dir}
+cp tool_train/ade20k/psp_train_sat.sh tool_train/train_sat_psp.py ${config} ${exp_dir}
 
 export PYTHONPATH=./
-export CUDA_HOME=/home/zhangyuxuan/cuda-11.1
 #srun -p $PARTITION -n1 --gres=gpu:8 --ntasks-per-node=8 --job-name=python \
 $PYTHON tool_train/train_sat_psp.py \
   --config=${config} \
