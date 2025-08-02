b527de497d1ecc9b93ececbb18d6faae74fba1a9
yuxuan357 <yxzhang0911@gmail.com>
2025-08-02 16:46:17 +0800

    Update aspp_train_sat.sh

--- tool_train/ade20k/aspp_train_sat.sh
+++ tool_train/ade20k/aspp_train_sat.sh
@@ -13,8 +13,6 @@ mkdir -p ${model_dir} ${result_dir}
 cp tool_train/ade20k/aspp_train_sat.sh tool_train/train_sat_aspp.py ${config} ${exp_dir}
 
 export PYTHONPATH=./
-export CUDA_HOME=/home/zhangyuxuan/cuda-11.1
-#srun -p $PARTITION -n1 --gres=gpu:8 --ntasks-per-node=8 --job-name=python \
 $PYTHON tool_train/train_sat_aspp.py \
   --config=${config} \
   2>&1 | tee ${model_dir}/train-$now.log
