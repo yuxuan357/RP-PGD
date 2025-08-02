e7efb617f8a637d8c47ae4193b1b4b25a5a3ea39
yuxuan357 <yxzhang0911@gmail.com>
2025-08-02 16:32:01 +0800

    Update psp_train_sat.sh

--- tool_train/ade20k/psp_train_sat.sh
+++ tool_train/ade20k/psp_train_sat.sh
@@ -13,6 +13,7 @@ mkdir -p ${model_dir} ${result_dir}
 cp tool_train/ade20k/psp_train_sat.sh tool_train/train_sat_psp.py ${config} ${exp_dir}
 
 export PYTHONPATH=./
+export CUDA_HOME=/home/zhangyuxuan/cuda-11.1
 #srun -p $PARTITION -n1 --gres=gpu:8 --ntasks-per-node=8 --job-name=python \
 $PYTHON tool_train/train_sat_psp.py \
   --config=${config} \
